d87e1feb7b9e480d9dba99b98c1d67d69e0272a4
travismorenz <46643435+travismorenz@users.noreply.github.com>
2019-12-19 10:33:57 -0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            components: path.resolve(__dirname, 'src/components')
+        }
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        })
+    ]
+};
