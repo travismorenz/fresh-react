eec8045efc52fc14162e2488038e11eeba9b4d70
Travis Morenz <Travis.Morenz@emsi-prec-tmorenz.emsi>
2020-01-06 16:20:00 -0800

    Can't avoid css and style loaders

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,10 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: {
